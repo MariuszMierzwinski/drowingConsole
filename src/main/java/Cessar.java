c54922bf6ca704202f53d0f0ae000c3ba9bfbfe1
mariusz <mariusz.mierzwinski@onet.pl>
2019-02-28 13:52:39 +0100

    sezar

---
+++ src/main/java/Cessar.java
@@ -0,0 +1,7 @@
+public class Cessar {
+    public static void main(String[] args) {
+        System.out.println(("A".hashCode()+", "+"a".hashCode()));
+        System.out.println((char)65+" "+(char)96);
+        System.out.println((int)'A'+ "A"+(int)'Z'+ "Z");
+    }
+}
