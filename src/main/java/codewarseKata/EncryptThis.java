dd24e652fd41887f64d3e6473ad464ec0447afa1
mariusz <mariusz.mierzwinski@onet.pl>
2019-04-02 13:10:07 +0200

    from mac

--- src/main/java/codewarseKata/EncryptThis.java
+++ src/main/java/codewarseKata/EncryptThis.java
@@ -4,14 +4,15 @@ import java.util.Arrays;
 
 public class EncryptThis {
     public static String encryptThis(String s) {
-        String str=s;
+        String str=s.trim();
         String tmp="";
         String[] arry = str.split(" ");
         if(s.length()!=0) {
             for (int i = 0; i < arry.length; i++) {
                 String[] chararry = arry[i].split("");
                 for (int j = 0; j < chararry.length; j++) {
-                    if (j == 0) {
+                    if(chararry[0].equals(" "))chararry[0] = " ";
+                    if ((j == 0)&&(!chararry[0].isEmpty())) {
                         chararry[0] = String.valueOf((int)(chararry[0].charAt(0)));
                     }
                     if ((j == 1)&&(chararry.length>2)) {
